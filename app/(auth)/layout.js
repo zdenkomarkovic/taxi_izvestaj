d17b6da83846e23ed09b183aca3deb8e39b829b7
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2024-12-22 01:04:32 +0100

    .

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,6 +1,10 @@
 import React from "react";
 const AuthLayout = ({ children }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-auth bg-cover bg-center bg-no-repeat px-4 py-10 ">
+      {children}
+    </main>
+  );
 };
 
 export default AuthLayout;
