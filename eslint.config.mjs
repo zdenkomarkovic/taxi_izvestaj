3f309634654ed17a53bc6c0ee15e8d3022b67217
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-12-21 11:49:47 +0100

    .

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    ignores: ["node_modules/**", ".next/**"],
+  },
+];
 
 export default eslintConfig;
