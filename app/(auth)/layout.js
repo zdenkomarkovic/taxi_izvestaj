83065331302fa9c464542ace0e0d55911c5750a1
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-12-21 03:46:07 +0100

    .

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,5 +1,14 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import React from "react";
-const AuthLayout = ({ children }) => {
+
+const AuthLayout = async ({ children }) => {
+  const session = await auth();
+
+  if (session) {
+    redirect("/");
+  }
+
   return (
     <main className="flex min-h-screen items-center justify-center bg-auth bg-cover bg-center bg-no-repeat px-4 py-10 ">
       {children}
--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,7 +1,15 @@
 import Navbar from "@/components/navbar/page";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Layout = ({ children }) => {
+const Layout = async ({ children }) => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/sign-in");
+  }
+
   return (
     <main>
       <Navbar />
